30d7a4da8ea38d8848ae71e3c89be395d57d5764
Cryogenic <rahultarak12345@gmail.com>
2016-03-19 21:42:46 +0530

    updated php

--- forum/index.php
+++ forum/index.php
@@ -4,10 +4,40 @@
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 -->
+<?php
+$Id = $_Get['Id'];
+$servername = "	ftp.0fees.us";
+$username = "0fe_17703085";
+$password = "cancer";
+$conn = new mysqli($servername, $username, $password);
+$sql = "SELECT * FROM Forum WHere Id =$Id";
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+			$effect = $row['Effect'];
+    $uid=$row['uid'];
+		$sql_1 = "SELECT Name FROM Users WHERE Id = $uid ";
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+				}
+  $user_name = $row['Name'];
+			}
+	} else {
+
+	}
+      }
+
+    }
+} else {
+
+}
+?>
 <html>
 	<head>
-		<title>No Sidebar - Phase Shift by TEMPLATED</title>
+		<title>Forum</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+			<base href="../../"></base>
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
 		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
@@ -31,15 +61,12 @@
 				<!-- Header -->
 					<div id="header" class="skel-panels-fixed">
 						<div id="logo">
-							<h1><a href="index.html">Phase Shift</a></h1>
-							<span class="tag">by TEMPLATED</span>
+							<h1><a href="index.html">Forum-Phorum</a></h1>
+							<span class="tag">One stop for your pharmaceutical problems</span>
 						</div>
 						<nav id="nav">
 							<ul>
-								<li class="active"><a href="index.html">Homepage</a></li>
-								<li><a href="left-sidebar.html">Left Sidebar</a></li>
-								<li><a href="right-sidebar.html">Right Sidebar</a></li>
-								<li><a href="no-sidebar.html">No Sidebar</a></li>
+
 							</ul>
 						</nav>
 					</div>
@@ -49,21 +76,19 @@
 					<div id="page" class="container">
 						<section>
 							<header class="major">
-								<h2>No Sidebar</h2>
-								<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
+								<h2>Forum</h2>
+								<span class="byline">By<?php echo $user_name; ?> </span>
 							</header>
-							<p>Maecenas pede nisl, elementum eu, ornare ac, malesuada at, erat. Proin gravida orci porttitor enim accumsan lacinia. Donec condimentum, urna non molestie semper, ligula enim ornare nibh, quis laoreet eros quam eget ante. Aliquam libero. Vivamus nisl nibh, iaculis vitae, viverra sit amet, ullamcorper vitae, turpis. Aliquam erat volutpat. Vestibulum dui sem, pulvinar sed, imperdiet nec, iaculis nec, leo. Fusce odio. Etiam arcu dui, faucibus eget, placerat vel, sodales eget, orci. Donec ornare neque ac sem. Mauris aliquet. Aliquam sem leo, vulputate sed, convallis at, ultricies quis, justo. Donec nonummy magna quis risus. Quisque eleifend. Phasellus tempor vehicula justo. Aliquam lacinia metus ut elit. Donec nonummy magna quis risus. Quisque eleifend. Maecenas pede nisl, elementum eu, ornare ac, malesuada at, erat. Proin gravida orci porttitor enim accumsan lacinia. Donec condimentum, urna non molestie semper, ligula enim ornare nibh, quis laoreet eros quam eget ante. Aliquam libero. Donec ornare neque ac sem. Mauris aliquet. Aliquam sem leo, vulputate sed, convallis at, ultricies quis, justo. Donec nonummy magna quis risus. Quisque eleifend. Phasellus tempor vehicula justo. Aliquam lacinia metus ut elit.</p>
-							<p>Vivamus nisl nibh, iaculis vitae, viverra sit amet, ullamcorper vitae, turpis. Aliquam erat volutpat. Vestibulum dui sem, pulvinar sed, imperdiet nec, iaculis nec, leo. Fusce odio. Etiam arcu dui, faucibus eget, placerat vel, sodales eget, orci. Donec ornare neque ac sem. Mauris aliquet. Aliquam sem leo, vulputate sed, convallis at, ultricies quis, justo. Phasellus tempor vehicula justo. Aliquam lacinia metus ut elit. Maecenas pede nisl, elementum eu, ornare ac, malesuada at, erat. Proin gravida orci porttitor enim accumsan lacinia. Donec condimentum, urna non molestie semper, ligula enim ornare nibh, quis laoreet eros quam eget ante. Aliquam libero. Vivamus nisl nibh, iaculis vitae, viverra sit amet, ullamcorper vitae, turpis. Donec ornare neque ac sem. Mauris aliquet. Aliquam sem leo, vulputate sed, convallis at, ultricies quis, justo. Donec nonummy magna quis risus. Quisque eleifend. Phasellus tempor vehicula justo. Aliquam lacinia metus ut elit. Aliquam erat volutpat. Vestibulum dui sem, pulvinar sed, imperdiet nec, iaculis nec, leo. Fusce odio. Etiam arcu dui, faucibus eget, placerat vel, sodales eget, orci. Donec ornare neque ac sem. Aliquam sem leo, vulputate sed, convallis at, ultricies quis, justo. Donec nonummy magna quis risus.</p>
-							<p>Vivamus nisl nibh, iaculis vitae, viverra sit amet, ullamcorper vitae, turpis. Aliquam erat volutpat. Vestibulum dui sem, pulvinar sed, imperdiet nec, iaculis nec, leo. Fusce odio. Etiam arcu dui, faucibus eget, placerat vel, sodales eget, orci. Donec ornare neque ac sem. Mauris aliquet. Aliquam sem leo, vulputate sed, convallis at, ultricies quis, justo. Phasellus tempor vehicula justo. Aliquam lacinia metus ut elit. Maecenas pede nisl, elementum eu, ornare ac, malesuada at, erat. Proin gravida orci porttitor enim accumsan lacinia. Donec condimentum, urna non molestie semper, ligula enim ornare nibh, quis laoreet eros quam eget ante. Aliquam libero. Vivamus nisl nibh, iaculis vitae, viverra sit amet, ullamcorper vitae, turpis. Donec ornare neque ac sem. Mauris aliquet. Aliquam sem leo, vulputate sed, convallis at, ultricies quis, justo. Donec nonummy magna quis risus. Quisque eleifend. Phasellus tempor vehicula justo. Aliquam lacinia metus ut elit. Aliquam erat volutpat. Vestibulum dui sem, pulvinar sed, imperdiet nec, iaculis nec, leo. Fusce odio. Etiam arcu dui, faucibus eget, placerat vel, sodales eget, orci. Mauris aliquet. Aliquam sem leo, vulputate sed, convallis at, ultricies quis, justo. Donec nonummy magna quis risus.</p>
-						</section>
+							<p><?php echo $effect; ?>
+							</P>				</section>
 					</div>
 				<!-- /Page -->
 
 				<!-- Main -->
 					<div id="main">
 						<div class="container">
-							<div class="row"> 
-								
+							<div class="row">
+
 								<!-- Content -->
 								<div class="6u">
 									<section>
@@ -92,7 +117,7 @@
 							</div>
 						</div>
 					</div>
-				<!-- /Main --> 
+				<!-- /Main -->
 
 	</div>
 
--- php/index.php
+++ php/index.php
@@ -1,10 +1,34 @@
-<!DOCTYPE html>
-<html>
-<body>
+  <?php
+$search = $_Get['search'];
+$servername = "	ftp.0fees.us";
+$username = "0fe_17703085";
+$password = "cancer";
 
-<? php
-echo "Hello world!";
- ?>
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-</body>
-</html>
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT Id, Drugs, Effects FROM Forum ORDER BY Id Desc";
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      $strpar = $row['Effects'];
+      $pos = strpos($strpar,$search);
+      if ($pos== true) {
+        $Id = $row['Id'];
+        header("Loacation: www.phorum.0fees.us/forum/index.php?Id=$Id ");
+        die();
+
+      }
+
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+?>
